e5582dfad3c1a98c14b7af5b2e9e81ca483e4155
Артур Филимонцев <homerboy19520@mail.ru>
2021-10-30 15:50:23 +0300

    push

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,6 @@
 const burgerIcon = document.querySelector(".header__burger-wrapper");
 const burgerWindow = document.querySelector(".header__list");
 const headerIcon = document.querySelector(".svg-header-svg");
-const body = document.querySelector("body");
 
 if (burgerIcon) {
   headerIcon.addEventListener("click", () => {
@@ -11,33 +10,4 @@ if (burgerIcon) {
   });
 }
 
-const input = document.querySelector(".header__input");
-const submit = document.querySelector(".header__button");
-
-const error = () => {
-  input.classList.add("header__input_eror");
-};
-
-const success = () => {
-  input.classList.remove("header__input_eror");
-};
-
-submit.addEventListener("click", () => {
-  let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-
-  if (input.value !== 0) {
-    if (!regex.test(input.value)) {
-      error();
-    } else {
-      success();
-    }
-  } else {
-    error();
-  }
-});
-
-input.onfocus = function() {
-  if (input.onfocus && input.classList.contains("header__input_eror")) {
-    input.classList.remove("header__input_eror");
-  }
-};
+validation(".header__input", ".header__button");
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,2 @@
 //= require components/**/*.js
+//= require elements/**/*.js
