b04adf5513cdc551b40bf76c7f7c64dd4f5113d4
Артур Филимонцев <homerboy19520@mail.ru>
2021-11-03 20:31:02 +0300

    push dev

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,14 @@
 const burgerIcon = document.querySelector(".header__burger-wrapper");
 const burgerWindow = document.querySelector(".header__list");
 const headerIcon = document.querySelector(".svg-header-svg");
+const body = document.querySelector(".body");
 
 if (burgerIcon) {
   headerIcon.addEventListener("click", () => {
     burgerWindow.classList.toggle("m-open");
     body.classList.toggle("body_hidden");
     headerIcon.classList.toggle("svg-header-svg_black");
+    console.log("work");
   });
 }
 
